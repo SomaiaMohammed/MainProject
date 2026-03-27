6636a8e83a62359f7694023340db4fd623ba5f38
SomaiaMohammed <40731966+SomaiaMohammed@users.noreply.github.com>
2026-03-27 05:15:50 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+import { Button, Input, Alert } from "./libs/ui-lib/index.js";
+
+const app = document.getElementById("app");
+
+app.appendChild(Button("Click Me"));
+app.appendChild(Input("Enter name"));
+app.appendChild(Alert("Error happened!"));
